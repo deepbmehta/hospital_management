b0e05b0f3de03c4c9f080f2081c42cb4233a43e0
Deep Mehta <deepmehta899@gmail.com>
2017-08-06 13:02:11 +0530

    added comma in urls.py

--- patient/urls.py
+++ patient/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from patient import views
 urlpatterns = [
-	url(r'^search/', views.search)	
+	url(r'^search/', views.search),	
 
 	url(r'^blood_bank/', views.bloodBank),
 	
