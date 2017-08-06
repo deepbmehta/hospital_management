425841e59df0a309399f56a79c0297da8f1d83ed
Gaurav Kataria <gaurav290497@gmail.com>
2017-08-06 12:21:17 +0530

    Bloodbank 90% done

--- patient/urls.py
+++ patient/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
+from patient import views
 urlpatterns = [
 	
 
-
+	url(r'^blood_bank/', views.bloodBank),
+	
 ]
--- patient/views.py
+++ patient/views.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+import json
 from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
 
 # Create your views here.
+def bloodBank(request):
+	with open('patient/bloodbank.json', 'r') as f:
+		hdata = json.load(f)
+		print hdata
+	return render(request,'bloodBank.html',hdata)
