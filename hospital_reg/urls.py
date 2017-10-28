9f04aeef9d8d79af0beb8a59f9ccbe9c4e1411e6
Deep Mehta <deepmehta899@gmail.com>
2017-10-28 15:35:45 +0530

    s

--- hospital_reg/urls.py
+++ hospital_reg/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from hospital_reg import views
 
 urlpatterns = [
-	url(r'^', views.homepage),
+	url(r'^$', views.homepage),
 	url(r'^login/$', views.login_site),
 	url(r'^logout/$', views.logout_site),
 	url(r'^hos_reg/$', views.hos_reg),
